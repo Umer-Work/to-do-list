d339aa2db16f802d67f8783b04a34b5619749216
umer-work <umer.webosmotic@gmail.com>
2024-12-06 10:59:22 +0530

    fix: 1. added console log for debugging

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,7 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log({ aa: import.meta.env });
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+console.log({ bb: supabaseUrl, cc: supabaseAnonKey });
 
 const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
 
